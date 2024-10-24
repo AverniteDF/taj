0f0379a6f39377a1c3fe102ed062ca4aba28c682
AverniteDF <115361770+AverniteDF@users.noreply.github.com>
2024-10-23 21:46:19 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -267,6 +267,7 @@ $date = new DateTime($timestamp, $utcTimezone);
 $date->setTimezone($estTimezone);
 $timestamp = $date->format('Y-m-d @ H:i T');
 
-$elapsed = round((microtime(true) - $startTime) * 1000); // End timer
+//$elapsed = round((microtime(true) - $startTime) * 1000); // End timer
+$elapsed = sprintf("%.2g", (microtime(true) - $startTime) * 1000); // End timer
 echo "\n\n<!-- Served by $serverSoftware Web Server & PHP " . phpversion() . " on $timestamp in $elapsed ms -->";
 ?>
